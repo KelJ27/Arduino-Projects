d985e9613182d98dec801187157949d2f0d98f1f
KelJ27 <36095100+KelJ27@users.noreply.github.com>
2024-06-08 16:20:53 -0400

    Added feature where LED color is changed when the joystick is changed. Also, made fixes to map calculations for the analog stick.

--- LED-Matrix-With-Analog-Stick.ino
+++ LED-Matrix-With-Analog-Stick.ino
@@ -1,16 +1,23 @@
-#define DATA_PIN        8    // Row data line
-#define CLOCK_PIN       9    // Clock sequence
-#define LATCH_PIN       10   // Latch pin
-#define MAX_ROWS        8    // Max number of rows in LED matrix
-#define JOY_BUTTON_PIN  2    // Button of the joystick
-#define JOY_X           A0   // X-axis of joystick
-#define JOY_Y           A1   // Y-axis of joystick
+#define DATA_PIN          8    // Row data line
+#define CLOCK_PIN         9    // Clock sequence
+#define LATCH_PIN         10   // Latch pin
+#define MAX_ROWS          8    // Max number of rows in LED matrix
+#define JOY_BUTTON_PIN    2    // Button of the joystick
+#define JOY_X             A0   // X-axis of joystick
+#define JOY_Y             A1   // Y-axis of joystick
+#define MAX_CLICK_COUNT   3    // Max number of times to click button
 
 const int NUM_ROWS = 8;
 const int NUM_COLUMNS = 8;
 int pic[MAX_ROWS] = {1,0,0,0,0,0,0,0};
 int bitNum[8] = {1,2,4,8,16,32,64,128};
-int xValue, yValue, xMap, yMap;
+double xValue, yValue, xMap, yMap;
+int clickCount = 0;
+unsigned long lastDebounceTime = 0;  // The last time the output pin was toggled
+unsigned long debounceDelay = 50;    // The debounce time
+int buttonState;                     // the current reading from the input pin
+int lastButtonState = LOW;           // the previous reading from the input pin
+int reading;
 
 // the setup function runs once when you press reset or power the board
 void setup() 
@@ -20,8 +27,9 @@ void setup()
   pinMode(DATA_PIN, OUTPUT);
   pinMode(CLOCK_PIN, OUTPUT);
   pinMode(LATCH_PIN, OUTPUT);
+  pinMode(JOY_BUTTON_PIN, INPUT_PULLUP);
 
-  //clearLedMatrix();
+  clearLedMatrix();
 }
 
 // the loop function runs over and over again forever
@@ -29,15 +37,21 @@ void loop()
 {
   xValue = analogRead(JOY_X);
   yValue = analogRead(JOY_Y);
-  xMap = map(xValue, 0, 1023, 0, 8);
-  yMap = map(yValue, 0, 1023, 0, 8);
+  xMap = mmap(xValue, 0, 1023, 0, 7);
+  yMap = mmap(yValue, 0, 1023, 0, 7);
  
   //print the values with to plot or view
-  Serial.print(xMap);
+  Serial.print(xValue);
   Serial.print("\t");
-  Serial.println(yMap);
+  Serial.println(yValue);
 
+  debounceButtonPress();
   setLedMatrixGrid();
+}
+
+long mmap(double x, double in_min, double in_max, double out_min, double out_max)
+{
+  return round((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
 }
 
 int binaryStringToDecimal(char *s) {
@@ -75,10 +89,23 @@ void setLedMatrixGrid()
 {
   for (int i = 0; i <8; i++)
   {
-
-    //useRedLeds();
-    //useGreenLeds();
-    useOrangeLeds();
+    switch(clickCount)
+    {
+      case 1:
+        useRedLeds();
+        break;
+      case 2:
+        useGreenLeds();
+        break;
+      case 3:
+        useOrangeLeds();
+        break;
+      default:
+        clickCount = 1;
+        useRedLeds();
+        break;
+    }
+    //useOrangeLeds();
     setRow(xMap);
     store();
   }  
@@ -100,6 +127,42 @@ void useOrangeLeds()
 {
   setColumn(yMap);
   setColumn(yMap);
+}
+
+void debounceButtonPress()
+{
+  reading = digitalRead(JOY_BUTTON_PIN);
+
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) 
+  {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) 
+  {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) 
+    {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) 
+      {
+        clickCount++;
+      }
+    }
+  }
+
+  lastButtonState = reading;
 }
 
 void clearLedMatrix()
